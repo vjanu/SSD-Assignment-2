075a038adc72351338ba732e1e0353df81f99683
vjanu <vjanuradhawick@gmail.com>
2019-09-06 18:10:26 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+     
+     // client id of the project
+
+     var clientId = "349805278143-c2vi2ruhvv8032fq9558sr111ohbn6pp.apps.googleusercontent.com";
+
+     // redirect_uri of the project
+
+     var redirect_uri = "http://localhost:8081/GDrive/upload.html";
+
+     // scope of the project
+
+     var scope = "https://www.googleapis.com/auth/drive";
+
+     // the url to which the user is redirected to
+
+     var url = "";
+
+
+     // this is event click listener for the button
+
+     $("#login").click(function(){
+
+        // this is the method which will be invoked it takes four parameters
+
+        signIn(clientId,redirect_uri,scope,url);
+
+     });
+
+     function signIn(clientId,redirect_uri,scope,url){
+      
+        // the actual url to which the user is redirected to 
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+        +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+        +"&access_type=offline";
+
+        // this line makes the user redirected to the url
+
+        window.location = url;
+
+
+
+
+     }
+
+
+
+});
